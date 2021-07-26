fa3e566796527b81d1e508eac492889e758b1da8
Wahed007 <neeaz.pratik@gmail.com>
2021-07-26 11:22:39 +0600

    commit

--- react-test/src/Person/Person.js
+++ react-test/src/Person/Person.js
@@ -5,7 +5,7 @@ import './Person.css'
 const person=(props)=>{
             return (
                 <div className='Person'>
-                <p onClick={props.clicked}>My name is not who i am {props.name}, I am {props.age} years old</p>
+                <p onClick={props.clicked}>My {props.name}, I am {props.age} years old</p>
                 <p>{props.children}</p>
                 <input type='text' onChange={props.changed} value={props.name}/>
                 </div>        
